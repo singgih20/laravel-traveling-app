1bee97a23c7b3123ea18db3f99cdd59e5f024f90
singgih20 <singgihajisasongko@student.telkomuniversity.ac.id>
2020-03-07 11:37:03 +0700

    travel app v0.1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TravelPackage;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $items = TravelPackage::with(['galleries'])->get();
+        return view('pages.home', compact('items'));
+    }
+}
